273e5d334fc5980ddcd994dd9b2b1ac9bf261d03
NoobDancer100 <perrinhillam@gmail.com>
2025-03-25 10:25:33 -0600

    Switch to hash mode due to github

--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,4 @@
-import { createWebHistory, createRouter } from 'vue-router'
+import { createWebHashHistory, createRouter } from 'vue-router'
 
 import Home from './pages/Home.vue'
 import ModList from './pages/ModList.vue'
@@ -9,7 +9,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes,
 })
 
