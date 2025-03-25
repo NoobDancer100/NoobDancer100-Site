86df2f7d4e75dfa5b1cd06c57018ae929ebbc44b
NoobDancer100 <perrinhillam@gmail.com>
2025-03-25 10:38:55 -0600

    Do some test things

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -4,6 +4,7 @@
 <template>
   <h1>NoobDancer100</h1>
   <p>The best developer of all time!</p>
+  <RouterLink to="/strangefuture-modlist">Strange Future ModList</RouterLink>
 </template>
 
 <style scoped>
